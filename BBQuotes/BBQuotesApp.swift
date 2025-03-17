95e6168139520e845e91ce6a7062248de7a458bd
Vaibhav Kulkarni <120665380+ivaibhavkulkarni@users.noreply.github.com>
2025-03-17 12:26:49 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Vaibhav kulkarni on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
